d0e43f12b8d1855c89f594d64a7fb9762b07932e
Andy <a.mcdermid.bryant@gmail.com>
2018-12-26 11:41:58 -0500

    working on resume

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ window.onscroll = function () {
 };
 
 function scroll() {
-    if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+    if (document.body.scrollTop > window.innerHeight * .85 || document.documentElement.scrollTop > window.innerHeight * .85) {
         document.getElementById("nav").style.height = "50px";
+        document.getElementById("name-plate").style.fontSize = '1.2rem';
     } else {
         document.getElementById("nav").style.height = "100px";
+        document.getElementById("name-plate").style.fontSize = '2rem';
     }
 }
