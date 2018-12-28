1fddf12c376b5cee74b30d22d9673765f0e0edbd
Andy <a.mcdermid.bryant@gmail.com>
2018-12-28 13:34:36 -0500

    v1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-window.onscroll = function () {
-    scroll()
-};
-
-function scroll() {
-    if (document.body.scrollTop > window.innerHeight * .85 || document.documentElement.scrollTop > window.innerHeight * .85) {
-        document.getElementById("nav").style.height = "50px";
-        document.getElementById("name-plate").style.fontSize = '1.2rem';
-    } else {
-        document.getElementById("nav").style.height = "100px";
-        document.getElementById("name-plate").style.fontSize = '2rem';
-    }
+function scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight);
 }
